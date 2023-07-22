55044e13ea3ec4f4aed63004bfae95527759da0a
alalulu8668 <wllei@kth.se>
2023-07-22 09:01:50 +0200

    Use scikit-image 0.19.3

--- scripts/parepare-scikit-image-docs.sh
+++ scripts/parepare-scikit-image-docs.sh
@@ -1,8 +1,7 @@
-VERSION=0.21.0
 # conda install -c conda-forge latexmk -y
 mkdir -p build
 cd build
 # if scikit-image folder not exist, clone it
-[ ! -d scikit-image ] && git clone https://github.com/scikit-image/scikit-image.git --branch v$VERSION --single-branch
+[ ! -d scikit-image ] && git clone https://github.com/scikit-image/scikit-image.git --branch v$SCIKIT_IMAGE_VERSION --single-branch
 cat ../scripts/latexpdf-fixed >> scikit-image/doc/Makefile
 
