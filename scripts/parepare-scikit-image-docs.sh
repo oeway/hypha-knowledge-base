db7ae7865b12d1621c3ff68e2608de22f07486d6
alalulu8668 <wllei@kth.se>
2023-07-22 07:09:17 +0200

    create patch script

--- scripts/parepare-scikit-image-docs.sh
+++ scripts/parepare-scikit-image-docs.sh
@@ -4,4 +4,5 @@ mkdir -p build
 cd build
 # if scikit-image folder not exist, clone it
 [ ! -d scikit-image ] && git clone https://github.com/scikit-image/scikit-image.git --branch v$VERSION --single-branch
+cat ../scripts/latexpdf-fixed >> scikit-image/doc/Makefile
 
