8b7e9098977187bedba14f2225d60615583b4e6a
alalulu8668 <wllei@kth.se>
2023-08-02 07:47:44 +0200

    Use FAISS instead of chroma

--- scripts/build_vector_database.py
+++ scripts/build_vector_database.py
@@ -1,22 +1,14 @@
 import os
-import openai
-from langchain.vectorstores import Chroma
+from langchain.vectorstores import FAISS
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.llms import OpenAI
-from langchain.chains import RetrievalQA
-from langchain.document_loaders import TextLoader
-from langchain.schema import Document
 from langchain.document_loaders import PyPDFLoader
-from tqdm import tqdm
-import chromadb
-assert chromadb.__version__ == "0.4.2"
 
 
 SCIKIT_IMAGE_VERSION = os.environ.get("SCIKIT_IMAGE_VERSION")
 assert SCIKIT_IMAGE_VERSION is not None, "Please set SCIKIT_IMAGE_VERSION environment variable"
 
-def create_embeddings_from_pdf(pdf_path, collection_name):
+def create_embeddings_from_pdf(pdf_path, index_name, output_dir):
     # convert pdf to texts
     loader = PyPDFLoader(pdf_path)
     # split texts into chunks
@@ -26,16 +18,15 @@ def create_embeddings_from_pdf(pdf_path, collection_name):
     # create embeddings
     embeddings = OpenAIEmbeddings()
     print(f"Creating embeddings (#documents={len(documents)}))")
-    vectordb = Chroma.from_documents(documents, embeddings, persist_directory="docs/vectordb", collection_name=collection_name)
+    vectordb = FAISS.from_documents(documents, embeddings)
+    vectordb.save_local(output_dir, index_name=index_name)
     return vectordb
 
 if __name__ == "__main__":
-    vectordb = create_embeddings_from_pdf(f"docs/scikit-image-{SCIKIT_IMAGE_VERSION}.pdf", collection_name="scikit-image")
-    vectordb.persist()
+    vectordb = create_embeddings_from_pdf(f"docs/scikit-image-{SCIKIT_IMAGE_VERSION}.pdf", index_name="scikit-image", "docs/vectordb")
     print("Embeddings created")
 
-
-    vectordb = Chroma(collection_name="scikit-image", persist_directory="docs/vectordb", embedding_function=OpenAIEmbeddings())
+    vectordb = FAISS.load_local(folder_path="docs/vectordb", index_name="scikit-image", embeddings=OpenAIEmbeddings())
     retriever = vectordb.as_retriever(score_threshold=0.4)
     items = retriever.get_relevant_documents("scikit-image release", verbose=True)
     print(items)
